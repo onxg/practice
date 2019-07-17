1ef0ff6090eab558c4b98acc394c7615f32fe4b7
onexgroup <kamil.lang@onexgroup.pl>
2019-07-17 13:22:51 +0200

    Stores view

---
+++ Practice/Practice.Core/Repositories/ISalesRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice.Core.Repositories
+{
+    using Practice.Core.ViewModels;
+    using System.Threading.Tasks;
+
+    public interface ISalesRepository
+    {
+        Task<DataTablesObject<Store>> GetAllStoresAsync(SearchFilters searchFilters);
+    }
+}
---
+++ Practice/Practice/Controllers/StoreController.cs
@@ -0,0 +1,72 @@
+﻿namespace Practice.Controllers
+{
+    using Practice.Core.Repositories;
+    using Practice.Core.ViewModels;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    public class StoreController : Controller
+    {
+        private readonly ISalesRepository saleRepository;
+
+        public StoreController(ISalesRepository _saleRepository)
+        {
+            saleRepository = _saleRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> GetAllStores(FormCollection form)
+        {
+            var searchFilters = new SearchFilters(form)
+            {
+                OrderBy = GetOrderBy(form)
+            };
+
+            var result = await saleRepository.GetAllStoresAsync(searchFilters);
+
+            return this.Json(
+                new
+                {
+                    iTotalRecords = result.iTotalRecords,
+                    iTotalDisplayRecords = result.iTotalDisplayRecords,
+                    aaData = result.aaData
+                }, JsonRequestBehavior.AllowGet);
+        }
+
+        private string GetOrderBy(FormCollection form)
+        {
+            string orderBy = form["order[0][column]"];
+
+            switch (orderBy)
+            {
+                case "0":
+                default:
+                    orderBy = "Name";
+                    break;
+                case "1":
+                    orderBy = "Address";
+                    break;
+                case "2":
+                    orderBy = "PostalCode";
+                    break;
+                case "3":
+                    orderBy = "City";
+                    break;
+                case "4":
+                    orderBy = "Country";
+                    break;
+                case "5":
+                    orderBy = "StatusId";
+                    break;
+            }
+
+            orderBy = (orderBy + " " + form["order[0][dir]"]).ToUpper();
+
+            return orderBy;
+        }
+    }
+}
