7e1d5b2994a6ccc3f22ae2070b28a271642ccfe2
tomaszrauk <tomaszrauk@icloud.com>
2019-07-17 11:48:31 +0200

    test

--- Practice/Practice/Controllers/EmployeeController.cs
+++ Practice/Practice/Controllers/EmployeeController.cs
@@ -1,6 +1,9 @@
 ﻿namespace Practice.Controllers
 {
     using Practice.Core.Repositories;
+    using System;
+    using System.Linq;
+    using System.Linq.Dynamic;
     using System.Threading.Tasks;
     using System.Web.Mvc;
 
@@ -13,11 +16,71 @@
             repository = _repository;
         }
 
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> GetEmployeesData()
+        {
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string serchValue = Request["search[value]"];
+            string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+            string sortDirection = Request["order[0][dir]"];
             var model = await repository.GetEmployees();
+            int totalRows = model.Count;
+            if (!string.IsNullOrEmpty(serchValue))
+            {
+                model = model.Where(x => x.LastName.ToLower().Contains(serchValue.ToLower())|| 
+                 x.FirstName.ToLower().Contains(serchValue.ToLower()) ||
+                 x.PhoneNumber.ToLower().Contains(serchValue.ToLower()) ||
+                 x.Address.ToLower().Contains(serchValue.ToLower()) ||
+                 x.PostalCode.ToLower().Contains(serchValue.ToLower()) ||
+                 x.City.ToLower().Contains(serchValue.ToLower()) ||
+                 x.Id.ToString().Contains(serchValue.ToLower()) ).ToList();
+            }
+
+              model = model.OrderBy(sortColumnName+" "+sortDirection).ToList();
+
+            int totalRowsAfterFiltering = model.Count;
+            model =  model.Skip(start).Take(length).ToList();
+            return Json(new { data = model,draw=Request["draw"],recordsTotal=totalRows,recordsFiltered= totalRowsAfterFiltering, JsonRequestBehavior.AllowGet });
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult> Delete(int? id)
+        {
+
+            if (!id.HasValue)
+            {
+                return RedirectToAction("Index");
+            }
+
+            await repository.DeleteEmployee(id.Value);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (!id.HasValue || id == 0)
+                return RedirectToAction("Index");
+
+            var model = await repository.GetEmployeeById(id.Value);
+            if(model == null)
+                return RedirectToAction("Index");
 
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Core.ViewModels.Employee employee)
+        {
+            if (!ModelState.IsValid)
+                return RedirectToAction("Edit", new { id = employee.Id });
+
+            await repository.UpdateEmployee(employee);
+            return RedirectToAction("Index");
         }
     }
 }
--- Practice/Practice/js/demo/datatables-demo.js
+++ Practice/Practice/js/demo/datatables-demo.js
@@ -1,4 +1,32 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
-});
+//$(document).ready(function() {
+//    $('#dataTable').DataTable();
+//});
+$(document).ready(function () {
+    $('#data-table-employees').DataTable({
+        "ajax": {
+            "url": "/Employee/GetEmployeesData",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "FirstName","name":"FirstName" },
+            { "data": "LastName", "name": "LastName"  },
+            { "data": "PhoneNumber", "name": "PhoneNumber" },
+            { "data": "Address", "name": "Address"  },
+            { "data": "PostalCode", "name": "PostalCode" },
+            { "data": "City", "name": "City" },
+            {
+                "data": "Id", "render": function (id) {
+                    return "<a class='btn btn-primary' href=/Employee/Edit/" + id + ">Edit</a> <a class='btn btn-info' href=/Employee/Details/" + id + ">Details</a> <a class='btn btn-danger' href=/Employee/Delete/" + id + ">Delete</a>"
+
+                },
+                "orderable": false,
+                "width":"210px"
+            }
+        ],
+        "serverSide": "true",
+        "order": [0, "asc"],
+        "processing":"true"
+    })
+})
