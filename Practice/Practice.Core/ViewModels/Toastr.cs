abb1380fcd898ecc658b8f9bf7524ccb327d6da2
kamiljaworski <kamilj610@gmail.com>
2019-07-19 13:32:10 +0200

    Add email confirmation and Toastr messages

---
+++ Practice/Practice.Core/ViewModels/Toastr.cs
@@ -0,0 +1,9 @@
+﻿namespace Practice.Core.ViewModels
+{
+    public class Toastr
+    {
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
--- Practice/Practice/App_Start/AutofacConfig.cs
+++ Practice/Practice/App_Start/AutofacConfig.cs
@@ -1,35 +1,55 @@
-﻿namespace Practice.App_Start
-{
-    using Autofac;
-    using Autofac.Integration.Mvc;
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
-    using Microsoft.AspNet.Identity.Owin;
-    using Microsoft.Owin.Security;
-    using Practice.DAL;
-    using Practice.DAL.Identity;
-    using Practice.Repository;
-    using System.Data.Entity;
-    using System.Linq;
-    using System.Web;
-    using System.Web.Mvc;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Practice.Core.Services;
+using Practice.DAL;
+using Practice.DAL.Identity;
+using Practice.Repository;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
 
+namespace Practice.App_Start
+{
     public class AutofacConfig
     {
         public static void Configure()
         {
             var builder = new ContainerBuilder();
 
-            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            // Context, user storage and auth services
             builder.RegisterType<Context>().InstancePerRequest();
-            builder.Register(ctx => new UserStore<ApplicationUser>(ctx.Resolve<Context>())).As<IUserStore<ApplicationUser, string>>();
-            builder.RegisterType<UserManager<ApplicationUser, string>>();
+            builder.Register(c => new UserStore<ApplicationUser>(c.Resolve<Context>())).As<IUserStore<ApplicationUser, string>>();
+            builder.RegisterType<EmailTokenProvider<ApplicationUser>>().As<IUserTokenProvider<ApplicationUser, string>>();
+
+            builder.Register(c => new UserManager<ApplicationUser, string>(c.Resolve<IUserStore<ApplicationUser, string>>()) { UserTokenProvider = c.Resolve<IUserTokenProvider<ApplicationUser, string>>() });
             builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
             builder.RegisterType<SignInManager<ApplicationUser, string>>();
+
+            // Repositories
             builder.RegisterAssemblyTypes(typeof(HumanResourcesRepository).Assembly)
                 .Where(t => t.Name.EndsWith("Repository"))
                 .AsImplementedInterfaces()
                 .InstancePerRequest();
+
+
+            // Services
+            var appSettings = WebConfigurationManager.AppSettings;
+            builder.Register(c => new EmailService(new SmtpClient(appSettings["mailHost"], int.Parse(appSettings["mailPort"]))
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(appSettings["mailAccount"], appSettings["mailPassword"])
+            })).As<IEmailService>();
+
+            // Controllers
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
             builder.RegisterFilterProvider();
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
